9c9047d38af130070e5c32c1dfee027607f64081
Josebratt <josebratt@gmail.com>
2021-11-18 13:25:30 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'cliente-dashboard',
+    appId: '1:199335518398:web:8ee53ca025648f957e3eef',
+    storageBucket: 'cliente-dashboard.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBU0dBhxQQCncPnNA2nAtPziGyM1Rc7mFE',
+    authDomain: 'cliente-dashboard.firebaseapp.com',
+    messagingSenderId: '199335518398',
+  },
   production: true
 };
