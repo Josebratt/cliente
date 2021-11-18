2f4456e1acc6207139c7baac04a844b0c92d65bf
Josebratt <josebratt@gmail.com>
2021-11-18 13:27:40 -0500

    Create add-client.component.ts

---
+++ src/app/components/add-client/add-client.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-client',
+  templateUrl: './add-client.component.html',
+  styles: [
+  ]
+})
+export class AddClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
