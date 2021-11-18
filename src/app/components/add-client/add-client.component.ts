af53aeaf939fdc65fa8e579655c03663952b5185
Josebratt <josebratt@gmail.com>
2021-11-18 18:14:57 -0500

    Update add-client.component.ts

--- src/app/components/add-client/add-client.component.ts
+++ src/app/components/add-client/add-client.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Client } from 'src/app/models/client';
 
 @Component({
   selector: 'app-add-client',
@@ -8,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddClientComponent implements OnInit {
 
+  client: Client = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    balance: 0
+  };
+
+  disableBlanceOnAdd: boolean = true;
+  @ViewChild('clientForm') form: any;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit( {value, valid}: NgForm){
+      console.log(value, valid);
+      
   }
 
 }
