738ea7727242211c07309c0d091da7cd671fc71f
Josebratt <josebratt@gmail.com>
2021-11-20 14:04:37 -0500

    Update edit-client.component.ts

--- src/app/components/edit-client/edit-client.component.ts
+++ src/app/components/edit-client/edit-client.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FlashMessagesService } from 'flash-messages-angular';
+import { Client } from 'src/app/models/client';
+import { ClientService } from 'src/app/services/client.service';
 @Component({
   selector: 'app-edit-client',
   templateUrl: './edit-client.component.html',
@@ -8,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditClientComponent implements OnInit {
 
-  constructor() { }
+    id: string = '';
+     client!: Client | null  ;
+
+    // client: Client = {
+    //   firstName: '',
+    //   lastName: '',
+    //   email: '',
+    //   phone: '',
+    //   balance: 0
+    // } 
+
+    disableBalanceOnEdit: boolean = true;
+
+  constructor(
+    private clientService: ClientService,
+    private flashMessage: FlashMessagesService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    // get id fron url
+    this.id = this.route.snapshot.params['id'];
+    // get client
+    this.clientService.getClient(this.id).subscribe(
+      client => this.client = client
+      );
+  }
+
+  onSubmit( {value, valid}: NgForm){
+    
   }
 
 }
